aea1fd464cb1c396d893341c1d8cd553074fc138
E-2-B <eduardo2b@usp.br>
2026-03-20 15:24:31 -0300

    Fechar jogo

--- codigo-fonte.asm
+++ codigo-fonte.asm
@@ -1,7 +1,16 @@
-    .data           # Segmento de dados
+	.data				# Segmento de dados
+	.align 0
+seeya:	.asciz "Obrigado por jogar!"	# Mensagem que será mostrada quando o jogador fechar o jogo.
 
+	.text				# Segmento de Código
+	.align 2
+	.globl main
+	
+main:
 
-    .text           # Segmento de CÃ³digo
-    .align 2
-    .globl main
-main:   
+exit:	la a0, seeya			# Muda o valor de a0 para o endereço do primeiro byte do string de rótulo "seeya"
+	addi a7, zero, 4		# Imprime o string encontrado em a0 na próxima chamada do sistema
+	ecall
+	
+	addi a7, zero, 10		# Encerra o programa na próxima chamada do sistema
+	ecall
