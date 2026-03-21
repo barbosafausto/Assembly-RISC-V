5dce7da102266f94abae32869161d27411c34fd1
Kire_minsoo <79205662+ViolinSoloist@users.noreply.github.com>
2026-03-21 09:41:37 -0300

    entrada usuário e branching
    Início da implementação do tratamento da entrada + branching a partir da entrada fornecida
    (incompleto)

--- codigo-fonte.asm
+++ codigo-fonte.asm
@@ -3,7 +3,7 @@
 	# --------- TEXTOS E FRASES ------------
 	
 	.align 0
-seeya:	.asciz "Obrigado por jogar!"	# Mensagem que será mostrada quando o jogador fechar o jogo.
+seeya:	.asciz "Obrigado por jogar!\n"	# Mensagem que será mostrada quando o jogador fechar o jogo.
 
 # Mensagem de boas vindas
 hola:   .asciz "Tchoo Tchoo! tá na hora de montar uns trem bão por aí >:D\n\n" 
@@ -25,25 +25,59 @@ mostrarMenu:
 	.ascii "4 - Listar trem.\n"
 	.ascii "5 - Buscar vagão.\n"
 	.asciz "6 - sair.\n\n"
-
-	.text				# Segmento de Código
+	
+	
 	.align 2
+	.text				# Segmento de Código
 	.globl main
 	
-main:	la a0, hola			# printa Texto de boas vindas
-	addi a7, zero, 4
+	
+	
+main:	# ---------- INICIALIZAÇÃO DAS VARIÁVEIS ----------
+	addi s0, zero, 0	# Ponteiro da locomotiva começa como 0/NULL
+	addi s1, zero, 1	# O jogo já começa com 1 vagão
+	addi s2, zero, 0	# Menu começa zerado por default
+	
+	# ------- INICIO DO JOGO ---------
+	la a0, hola			# Muda o valor de a0 para o endereço do primeiro byte do string de rótulo "hola"
+	addi a7, zero, 4		# Imprime o string encontrado em a0 na próxima chamada do sistema, que é a mensagem de boas vindas
 	ecall
 	
 	la a0, instrucoes		# printa ao usuário como jogar
 	addi a7, zero, 4
 	ecall
 	
+interface:	
 	la a0, mostrarMenu		# printa o meno de ações
 	addi a7, zero, 4
 	ecall
+	
+get_input:
+	addi a7, zero, 5		# código de serviço ReadInt
+	ecall				# resultado fica automaticamente em a0
+	addi s2, zero, a0		# ENTRADA FICA SALVA EM s2
+	
+branch_from_input:			# ve qual é a entrada e pula pra função correspondente
 
-exit:	la a0, seeya			# Muda o valor de a0 para o endereço do primeiro byte do string de rótulo "seeya"
-	addi a7, zero, 4		# Imprime o string encontrado em a0 na próxima chamada do sistema
+	# 1 - Adicionar no início
+	addi t0, zero, 1			
+	beq s2, t0, opcao_1		# Se s2 == 1, pule para opcao_1
+	
+	# O usuário escolheu 2 (Adicionar no final)?
+	li t0, 2			# Coloca 2 na balança (t0)
+	beq s2, t0, opcao_2		# Se s2 == 2, pule para opcao_2
+	
+	# O usuário escolheu 6 (Sair)?
+	li t0, 6			# Coloca 6 na balança
+	beq s2, t0, exit		# Se s2 == 6, pule para a sua função exit
+	
+	# E SE ELE DIGITAR 9? (Tratamento de erro)
+	# Se o processador chegar até esta linha, significa que nenhum dos 'beq' acima funcionou.
+	# Aqui você pode imprimir "Opção Inválida" e pular de volta para o menu!
+	j interface			# Volta lá para cima para imprimir o menu de novo!
+
+exit:	la a0, seeya			# printa mensagem de despedida 
+	addi a7, zero, 4		
 	ecall
 	
 	addi a7, zero, 10		# Encerra o programa na próxima chamada do sistema
